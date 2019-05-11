cbb8d6c47aab76d29a0d7efd5fe7b703e3305184
Álvaro Millán Álvarez <amillan@hiberus.local>
2019-05-11 09:32:09 +0200

    añadir detalle de fotografia

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
+from photos.models import Photo
+
+
+def latest_photos(request):
+    photos = Photo.objects.all().order_by('-modification_date')
+
+    context = {'latest_photos': photos}
+
+    html = render(request, 'photos/latest.html', context)
+
+    return HttpResponse(html)
+
+def photo_detail(request, pk):
+    try:
+        photo = Photo.objects.get(pk=pk)
+    except Photo.DoesNotExist:
+        return HttpResponseNotFound()
+
+    context = {'photo': photo}
+
+    html = render(request, 'photos/detail.html', context)
+
+    return HttpResponse(html)
